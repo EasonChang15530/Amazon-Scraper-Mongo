5bc043dc29b13168575e0c0f970ca3a373180735
eason <eason15530@gmail.com>
2019-12-11 13:53:44 -0500

    fix render('index') handlebars bug

--- server.js
+++ server.js
@@ -9,63 +9,62 @@ var cheerio = require("cheerio");
 // Initialize Express
 var app = express();
 
-// Serve static content from the "public" directory in this application directory.
-app.use(express.static("public"));
-
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Serve static content from the "public" directory in this application directory.
+app.use(express.static("public"));
 
 // Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Database configuration
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+// // Database configuration
+// var databaseUrl = "scraper";
+// var collections = ["scrapedData"];
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function (error) {
-  console.log("Database Error:", error);
-});
+// // Hook mongojs configuration to the db variable
+// var db = mongojs(databaseUrl, collections);
+// db.on("error", function (error) {
+//   console.log("Database Error:", error);
+// });
 
 // Main route (simple Hello World Message)
-app.get("/", function (req, res) {
-  // res.send("Hello world");
-  res.render("index", { result })
+app.get('/', function (req, res) {
+  res.render('index');
 });
 
-app.get("/displayJSON", function (req, res) {
+// app.get("/displayJSON", function (req, res) {
 
-  db.scrapedData.find({}, function (error, found) {
-    if (error) {
-      console.log(error);
-    } else {
-      res.json(found);
-    }
-  });
-});
+//   db.scrapedData.find({}, function (error, found) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       res.json(found);
+//     }
+//   });
+// });
 
-app.get("/scrape", function (req, res) {
+// app.get("/scrape", function (req, res) {
 
-  axios.get("https://www.nytimes.com").then(function (response) {
-    var $ = cheerio.load(response.data);
-    // var results = [];
-    $("article").each(function (i, element) {
-      var title = $(element).children().text();
-      var link = $(element).find("a").attr("href");
+//   axios.get("https://www.nytimes.com").then(function (response) {
+//     var $ = cheerio.load(response.data);
+//     // var results = [];
+//     $("article").each(function (i, element) {
+//       var title = $(element).children().text();
+//       var link = $(element).find("a").attr("href");
 
-      var result = {
-        title: title,
-        link: link
-      };
-      console.log(result);
-      db.scrapedData.insert(result);
-    });
-  });
-})
-/* -/-/-/-/-/-/-/-/-/-/-/-/- */
+//       var result = {
+//         title: title,
+//         link: link
+//       };
+//       console.log(result);
+//       db.scrapedData.insert(result);
+//     });
+//   });
+// })
+// /* -/-/-/-/-/-/-/-/-/-/-/-/- */
 
 // Start this server so that it can begin listening to client requests.
 app.listen(3000, function() {
